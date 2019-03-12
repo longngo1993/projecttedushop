a2309e063ec123d9fe733b70ae1bcb1c32b6eb14
longnv1993 <ngovanlong2510@gmail.com>
2019-03-13 00:50:14 +0700

    Commit CKFinder

--- TeduShop.Data/Repositories/ProductRepository.cs
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -8,14 +8,19 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    public interface IProductRepository
+    public interface IProductRepository : IRepository<Product>
     {
+        IEnumerable<Product> GetByAlias(string alias);
     }
 
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
         {
+        }
+        public IEnumerable<Product> GetByAlias(string alias)
+        {
+            return this.DbContext.Products.Where(x => x.Alias == alias);
         }
     }
 }
